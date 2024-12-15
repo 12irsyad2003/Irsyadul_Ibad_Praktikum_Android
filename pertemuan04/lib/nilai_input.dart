fbcdd1bce420a075c14a38848375a3c8bfb17e1a
12irsyad2003 <irsyad2210010646@gmail.com>
2024-12-16 00:44:33 +0800

    menambahkan nama dan Npm nilai_input.dart

--- pertemuan04/lib/nilai_input.dart
+++ pertemuan04/lib/nilai_input.dart
@@ -31,6 +31,10 @@ class _NilaiMatakuliahPageState extends State<NilaiMatakuliahPage> {
   List<String?> selectedMatakuliah = List.filled(10, null);
   Konversi konversi = Konversi();
 
+  // Variabel untuk Nama dan NPM
+  String? nama;
+  String? npm;
+
   // Fungsi untuk menghitung IPK dan total SKS
   Map<String, double> hitungIPKdanSks() {
     double totalBobot = 0.0;
@@ -38,11 +42,9 @@ class _NilaiMatakuliahPageState extends State<NilaiMatakuliahPage> {
 
     for (int i = 0; i < matakuliah.length; i++) {
       if (selectedNilai[i] != null && selectedMatakuliah[i] != null) {
-        // Menghitung total bobot berdasarkan nilai huruf dan SKS
         double bobotNilai = konversi.nilaiKeAngka(selectedNilai[i]!);
-        totalBobot +=
-            bobotNilai * sksMatakuliah[i]; // Mengalikan bobot nilai dengan SKS
-        totalSks += sksMatakuliah[i]; // Menjumlahkan SKS
+        totalBobot += bobotNilai * sksMatakuliah[i];
+        totalSks += sksMatakuliah[i];
       }
     }
 
@@ -51,7 +53,6 @@ class _NilaiMatakuliahPageState extends State<NilaiMatakuliahPage> {
     return {'IPK': ipk, 'Total SKS': totalSks};
   }
 
-  // Fungsi untuk mendapatkan list pilihan mata kuliah yang masih tersedia
   List<String> getAvailableMatakuliah(int index) {
     List<String> availableMatakuliah = List.from(matakuliah);
     for (int i = 0; i < selectedMatakuliah.length; i++) {
@@ -68,86 +69,116 @@ class _NilaiMatakuliahPageState extends State<NilaiMatakuliahPage> {
       appBar: AppBar(title: Text('Input Nilai Matakuliah')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: matakuliah.length,
-          itemBuilder: (context, index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Mata Kuliah: ',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                // Dropdown untuk memilih Mata Kuliah
-                DropdownButtonFormField<String>(
-                  value: selectedMatakuliah[index],
-                  decoration: InputDecoration(labelText: 'Mata Kuliah'),
-                  items: getAvailableMatakuliah(index).map((matkul) {
-                    return DropdownMenuItem<String>(
-                      value: matkul,
-                      child: Text(matkul),
+        child: ListView(
+          children: <Widget>[
+            // Input Nama
+            TextField(
+              decoration: InputDecoration(labelText: 'Nama'),
+              onChanged: (value) {
+                setState(() {
+                  nama = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            // Input NPM
+            TextField(
+              decoration: InputDecoration(labelText: 'NPM'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  npm = value;
+                });
+              },
+            ),
+            SizedBox(height: 16),
+            // Input Mata Kuliah dan Nilai
+            ListView.builder(
+              itemCount: matakuliah.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Mata Kuliah: ',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    DropdownButtonFormField<String>(
+                      value: selectedMatakuliah[index],
+                      decoration: InputDecoration(labelText: 'Mata Kuliah'),
+                      items: getAvailableMatakuliah(index).map((matkul) {
+                        return DropdownMenuItem<String>(
+                          value: matkul,
+                          child: Text(matkul),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedMatakuliah[index] = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      value: selectedNilai[index],
+                      decoration: InputDecoration(labelText: 'Nilai'),
+                      items: ['A', 'B+', 'B', 'C+', 'C', 'D', 'E'].map((nilai) {
+                        return DropdownMenuItem<String>(
+                          value: nilai,
+                          child: Text(nilai),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedNilai[index] = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                  ],
+                );
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (selectedNilai.contains(null) ||
+                      selectedMatakuliah.contains(null) ||
+                      nama == null ||
+                      npm == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text("Harap lengkapi semua data!"),
+                    ));
+                  } else {
+                    var hasil = hitungIPKdanSks();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HasilIpkPage(
+                          ipk: hasil['IPK']!,
+                          totalSks: hasil['Total SKS']!,
+                          selectedMatakuliah: selectedMatakuliah,
+                          sksMatakuliah: sksMatakuliah,
+                          selectedNilai: selectedNilai,
+                          nama: nama!,
+                          npm: npm!,
+                        ),
+                      ),
                     );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedMatakuliah[index] = value;
-                    });
-                  },
+                  }
+                },
+                child: Text('Hitung Nilai'),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  textStyle: TextStyle(fontSize: 18),
+                  backgroundColor: Color.fromARGB(255, 127, 197, 255),
                 ),
-                SizedBox(height: 16),
-                // Dropdown untuk memilih Nilai
-                DropdownButtonFormField<String>(
-                  value: selectedNilai[index],
-                  decoration: InputDecoration(labelText: 'Nilai'),
-                  items: ['A', 'B+', 'B', 'C+', 'C', 'D', 'E'].map((nilai) {
-                    return DropdownMenuItem<String>(
-                      value: nilai,
-                      child: Text(nilai),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedNilai[index] = value;
-                    });
-                  },
-                ),
-                SizedBox(height: 16),
-              ],
-            );
-          },
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ElevatedButton(
-          onPressed: () {
-            if (selectedNilai.contains(null) ||
-                selectedMatakuliah.contains(null)) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text("Harap lengkapi semua mata kuliah dan nilai!"),
-              ));
-            } else {
-              var hasil = hitungIPKdanSks();
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HasilIpkPage(
-                    ipk: hasil['IPK']!,
-                    totalSks: hasil['Total SKS']!,
-                    selectedMatakuliah: selectedMatakuliah,
-                    sksMatakuliah: sksMatakuliah,
-                    selectedNilai: selectedNilai,
-                  ),
-                ),
-              );
-            }
-          },
-          child: Text('Hitung Nilai'),
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.symmetric(vertical: 16),
-            textStyle: TextStyle(fontSize: 18),
-            backgroundColor: Color.fromARGB(255, 127, 197, 255)
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
